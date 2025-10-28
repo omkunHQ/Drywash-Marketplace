11fa5763cfbd1fd5f548c1d35390ead7dae1bcb3
omkunHQ <omkunhq@gmail.com>
2025-10-28 08:19:46 +0530

    Update Firebase-Configuration.js

--- Firebase-Configuration.js
+++ Firebase-Configuration.js
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js"; // Import GoogleAuthProvider
